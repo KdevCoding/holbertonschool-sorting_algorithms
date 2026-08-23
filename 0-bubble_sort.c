2f0faddc3e4a847b07ef311a3c666d2e606182b6
Malakai <gam3rsuknight@gmail.com>
2026-08-23 22:56:58 +1000

    funcs for task

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	int swapped;
+	size_t i;
+	int temp;
+
+	swapped = 1;
+	while (swapped)
+	{
+		swapped = 0;
+		i = 1;
+		while (i < size)
+		{
+			if (array[i - 1] > array[i])
+			{
+				temp = array[i - 1];
+				array[i - 1] = array[i];
+				array[i] = temp;
+				swapped = 1;
+
+				print_array(array, size);
+			}
+
+			i++;
+		}
+	}
+}
